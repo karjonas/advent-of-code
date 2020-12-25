24358a849c5056ba3cbb1c18537bb2f8b091ca25
Jonas Karlsson <jonaskarlsson@fripost.org>
2020-12-25 10:27:33 +0100

    2020 Day 25

--- 2020/day25/src/lib.rs
+++ 2020/day25/src/lib.rs
@@ -1,3 +1,50 @@
 extern crate common;
 
-pub fn solve() {}
+type Input = (usize, usize);
+
+fn parse_input(input: &String) -> Input {
+    let numbers: Vec<_> = input.lines().map(|v| v.parse::<usize>().unwrap()).collect();
+    return (numbers[0], numbers[1]);
+}
+
+fn encrypt_subject(subject_number: usize, loop_size: usize) -> usize {
+    let mut value = 1;
+    for _ in 0..loop_size {
+        value = (value * subject_number) % 20201227;
+    }
+    return value;
+}
+
+fn find_loop_size(key: usize) -> usize {
+    let mut value = 1;
+    for loop_size in 1.. {
+        value = (value * 7) % 20201227;
+
+        if value == key {
+            return loop_size;
+        }
+    }
+    panic!("No loop size found");
+}
+
+fn part_one(input: &Input) -> usize {
+    return encrypt_subject(input.0, find_loop_size(input.1));
+}
+
+pub fn solve() {
+    let input = common::read_file("2020/day25/input");
+    println!("Part one: {}", part_one(&parse_input(&input)));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_samples() {
+        assert_eq!(find_loop_size(5764801), 8);
+        assert_eq!(find_loop_size(17807724), 11);
+        assert_eq!(encrypt_subject(17807724, 8), 14897079);
+        assert_eq!(encrypt_subject(5764801, 11), 14897079);
+    }
+}
