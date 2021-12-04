4929ab3b8864fd2093f95c0e9a69a0d9b0ac1ba3
Jonas Karlsson <jonaskarlsson@fripost.org>
2021-12-04 15:17:57 +0100

    2021 boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,140 +1,3 @@
-// Year 2015
-extern crate year2015day01;
-extern crate year2015day02;
-extern crate year2015day03;
-extern crate year2015day04;
-extern crate year2015day05;
-extern crate year2015day06;
-extern crate year2015day07;
-extern crate year2015day08;
-extern crate year2015day09;
-extern crate year2015day10;
-extern crate year2015day11;
-extern crate year2015day12;
-extern crate year2015day13;
-extern crate year2015day14;
-extern crate year2015day15;
-extern crate year2015day16;
-extern crate year2015day17;
-extern crate year2015day18;
-extern crate year2015day19;
-extern crate year2015day20;
-extern crate year2015day21;
-extern crate year2015day22;
-extern crate year2015day23;
-extern crate year2015day24;
-extern crate year2015day25;
-
-// Year 2016
-extern crate year2016day01;
-extern crate year2016day02;
-extern crate year2016day03;
-extern crate year2016day04;
-extern crate year2016day05;
-extern crate year2016day06;
-extern crate year2016day07;
-extern crate year2016day08;
-extern crate year2016day09;
-extern crate year2016day10;
-extern crate year2016day11;
-extern crate year2016day12;
-extern crate year2016day13;
-extern crate year2016day14;
-extern crate year2016day15;
-extern crate year2016day16;
-extern crate year2016day17;
-extern crate year2016day18;
-extern crate year2016day19;
-extern crate year2016day20;
-extern crate year2016day21;
-extern crate year2016day22;
-extern crate year2016day23;
-extern crate year2016day24;
-extern crate year2016day25;
-
-// Year 2017
-extern crate year2017day01;
-extern crate year2017day02;
-extern crate year2017day03;
-extern crate year2017day04;
-extern crate year2017day05;
-extern crate year2017day06;
-extern crate year2017day07;
-extern crate year2017day08;
-extern crate year2017day09;
-extern crate year2017day10;
-extern crate year2017day11;
-extern crate year2017day12;
-extern crate year2017day13;
-extern crate year2017day14;
-extern crate year2017day15;
-extern crate year2017day16;
-extern crate year2017day17;
-extern crate year2017day18;
-extern crate year2017day19;
-extern crate year2017day20;
-extern crate year2017day21;
-extern crate year2017day22;
-extern crate year2017day23;
-extern crate year2017day24;
-extern crate year2017day25;
-
-// Year 2018
-extern crate year2018day01;
-extern crate year2018day02;
-extern crate year2018day03;
-extern crate year2018day04;
-extern crate year2018day05;
-extern crate year2018day06;
-extern crate year2018day07;
-extern crate year2018day08;
-extern crate year2018day09;
-extern crate year2018day10;
-extern crate year2018day11;
-extern crate year2018day12;
-extern crate year2018day13;
-extern crate year2018day14;
-extern crate year2018day15;
-extern crate year2018day16;
-extern crate year2018day17;
-extern crate year2018day18;
-extern crate year2018day19;
-extern crate year2018day20;
-extern crate year2018day21;
-extern crate year2018day22;
-extern crate year2018day23;
-extern crate year2018day24;
-extern crate year2018day25;
-
-// Year 2019
-extern crate year2019day01;
-extern crate year2019day02;
-extern crate year2019day03;
-extern crate year2019day04;
-extern crate year2019day05;
-extern crate year2019day06;
-extern crate year2019day07;
-extern crate year2019day08;
-extern crate year2019day09;
-extern crate year2019day10;
-extern crate year2019day11;
-extern crate year2019day12;
-extern crate year2019day13;
-extern crate year2019day14;
-extern crate year2019day15;
-extern crate year2019day16;
-extern crate year2019day17;
-extern crate year2019day18;
-extern crate year2019day19;
-extern crate year2019day20;
-extern crate year2019day21;
-extern crate year2019day22;
-extern crate year2019day23;
-extern crate year2019day24;
-extern crate year2019day25;
-
-extern crate common;
-
 extern crate clap;
 
 use clap::{App, Arg};
@@ -161,6 +24,9 @@ fn solve_day(year: usize, day: usize) {
             if year == 2020 {
                 year2020day01::solve();
             }
+            if year == 2021 {
+                year2021day01::solve();
+            }
         }
         2 => {
             if year == 2015 {
@@ -180,6 +46,9 @@ fn solve_day(year: usize, day: usize) {
             }
             if year == 2020 {
                 year2020day02::solve();
+            }
+            if year == 2021 {
+                year2021day02::solve();
             }
         }
         3 => {
@@ -201,6 +70,9 @@ fn solve_day(year: usize, day: usize) {
             if year == 2020 {
                 year2020day03::solve();
             }
+            if year == 2021 {
+                year2021day03::solve();
+            }
         }
         4 => {
             if year == 2015 {
@@ -220,6 +92,9 @@ fn solve_day(year: usize, day: usize) {
             }
             if year == 2020 {
                 year2020day04::solve();
+            }
+            if year == 2021 {
+                year2021day04::solve();
             }
         }
         5 => {
@@ -241,6 +116,9 @@ fn solve_day(year: usize, day: usize) {
             if year == 2020 {
                 year2020day05::solve();
             }
+            if year == 2021 {
+                year2021day05::solve();
+            }
         }
         6 => {
             if year == 2015 {
@@ -260,6 +138,9 @@ fn solve_day(year: usize, day: usize) {
             }
             if year == 2020 {
                 year2020day06::solve();
+            }
+            if year == 2021 {
+                year2021day06::solve();
             }
         }
         7 => {
@@ -281,6 +162,9 @@ fn solve_day(year: usize, day: usize) {
             if year == 2020 {
                 year2020day07::solve();
             }
+            if year == 2021 {
+                year2021day07::solve();
+            }
         }
         8 => {
             if year == 2015 {
@@ -300,6 +184,9 @@ fn solve_day(year: usize, day: usize) {
             }
             if year == 2020 {
                 year2020day08::solve();
+            }
+            if year == 2021 {
+                year2021day08::solve();
             }
         }
         9 => {
@@ -321,6 +208,9 @@ fn solve_day(year: usize, day: usize) {
             if year == 2020 {
                 year2020day09::solve();
             }
+            if year == 2021 {
+                year2021day09::solve();
+            }
         }
         10 => {
             if year == 2015 {
@@ -340,6 +230,9 @@ fn solve_day(year: usize, day: usize) {
             }
             if year == 2020 {
                 year2020day10::solve();
+            }
+            if year == 2021 {
+                year2021day10::solve();
             }
         }
         11 => {
@@ -361,6 +254,9 @@ fn solve_day(year: usize, day: usize) {
             if year == 2020 {
                 year2020day11::solve();
             }
+            if year == 2021 {
+                year2021day11::solve();
+            }
         }
         12 => {
             if year == 2015 {
@@ -380,6 +276,9 @@ fn solve_day(year: usize, day: usize) {
             }
             if year == 2020 {
                 year2020day12::solve();
+            }
+            if year == 2021 {
+                year2021day12::solve();
             }
         }
         13 => {
@@ -401,6 +300,9 @@ fn solve_day(year: usize, day: usize) {
             if year == 2020 {
                 year2020day13::solve();
             }
+            if year == 2021 {
+                year2021day13::solve();
+            }
         }
         14 => {
             if year == 2015 {
@@ -420,6 +322,9 @@ fn solve_day(year: usize, day: usize) {
             }
             if year == 2020 {
                 year2020day14::solve();
+            }
+            if year == 2021 {
+                year2021day14::solve();
             }
         }
         15 => {
@@ -441,6 +346,9 @@ fn solve_day(year: usize, day: usize) {
             if year == 2020 {
                 year2020day15::solve();
             }
+            if year == 2021 {
+                year2021day15::solve();
+            }
         }
         16 => {
             if year == 2015 {
@@ -460,6 +368,9 @@ fn solve_day(year: usize, day: usize) {
             }
             if year == 2020 {
                 year2020day16::solve();
+            }
+            if year == 2021 {
+                year2021day16::solve();
             }
         }
         17 => {
@@ -481,6 +392,9 @@ fn solve_day(year: usize, day: usize) {
             if year == 2020 {
                 year2020day17::solve();
             }
+            if year == 2021 {
+                year2021day17::solve();
+            }
         }
         18 => {
             if year == 2015 {
@@ -500,6 +414,9 @@ fn solve_day(year: usize, day: usize) {
             }
             if year == 2020 {
                 year2020day18::solve();
+            }
+            if year == 2021 {
+                year2021day18::solve();
             }
         }
         19 => {
@@ -521,6 +438,9 @@ fn solve_day(year: usize, day: usize) {
             if year == 2020 {
                 year2020day19::solve();
             }
+            if year == 2021 {
+                year2021day19::solve();
+            }
         }
         20 => {
             if year == 2015 {
@@ -540,6 +460,9 @@ fn solve_day(year: usize, day: usize) {
             }
             if year == 2020 {
                 year2020day20::solve();
+            }
+            if year == 2021 {
+                year2021day20::solve();
             }
         }
         21 => {
@@ -561,6 +484,9 @@ fn solve_day(year: usize, day: usize) {
             if year == 2020 {
                 year2020day21::solve();
             }
+            if year == 2021 {
+                year2021day21::solve();
+            }
         }
         22 => {
             if year == 2015 {
@@ -580,6 +506,9 @@ fn solve_day(year: usize, day: usize) {
             }
             if year == 2020 {
                 year2020day22::solve();
+            }
+            if year == 2021 {
+                year2021day22::solve();
             }
         }
         23 => {
@@ -601,6 +530,9 @@ fn solve_day(year: usize, day: usize) {
             if year == 2020 {
                 year2020day23::solve();
             }
+            if year == 2021 {
+                year2021day23::solve();
+            }
         }
         24 => {
             if year == 2015 {
@@ -621,6 +553,9 @@ fn solve_day(year: usize, day: usize) {
             if year == 2020 {
                 year2020day24::solve();
             }
+            if year == 2021 {
+                year2021day24::solve();
+            }
         }
         25 => {
             if year == 2015 {
@@ -640,6 +575,9 @@ fn solve_day(year: usize, day: usize) {
             }
             if year == 2020 {
                 year2020day25::solve();
+            }
+            if year == 2021 {
+                year2021day25::solve();
             }
         }
         _ => {}
@@ -679,7 +617,7 @@ fn main() {
     let year = if common::is_number(year_str) {
         common::string_to_usize(year_str)
     } else {
-        2020
+        2021
     };
 
     if day > 25 {
@@ -687,7 +625,7 @@ fn main() {
         std::process::exit(-1);
     }
 
-    if year > 2020 || year < 2015 {
+    if year > 2021 || year < 2015 {
         println!("Invalid year '{}'", year);
         std::process::exit(-1);
     }
