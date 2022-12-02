a6a481e9201bfcd33ae169167b6f627093106d88
Jonas Karlsson <jonaskarlsson@fripost.org>
2022-12-02 20:35:00 +0100

    2022 Day 01

--- 2022/day01/src/lib.rs
+++ 2022/day01/src/lib.rs
@@ -1,4 +1,12 @@
 extern crate common;
 
 pub fn solve() {
+    let input = common::read_file("2022/day01/input");
+    let mut calories: Vec<usize> = input
+        .split("\n\n")
+        .map(|nums| nums.split('\n').map(|v| v.parse::<usize>().unwrap()).sum())
+        .collect();
+    calories.sort_by(|a, b| b.cmp(a));
+    println!("Part one: {}", calories[0]);
+    println!("Part two: {}", calories[0] + calories[1] + calories[2]);
 }
